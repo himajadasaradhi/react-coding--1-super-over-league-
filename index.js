160284c1d416f669c1f4e7618962bd2f7e9022e5
himaja dasaradhi <himajadasaradhi2000@gmail.com>
2023-10-10 15:25:23 +0000

    committing files

---
+++ index.js
@@ -0,0 +1,11 @@
+const element = (
+  <div className="background">
+    <h1>Super Over League</h1>
+    <div className="images">
+      <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" />
+      <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
